3b345c972d59bac6aa4a96a17ce8ff3739f5da2a
hsanch13 <hayleysanchez13@gmail.com>
2023-11-02 18:30:51 -0600

    added functions

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(work = "go to the office"){
+    return `This Monday, I will ${work}.`
+}
+
+
+function wrapAdjective(flair = "*") {
+    return function (description = "special") {
+      return `You are ${flair}${description}${flair}!`
+    }
+  }
+  
